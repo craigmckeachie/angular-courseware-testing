361e02613ac97f570dd507e6d93eb5db26eb1666
Craig McKeachie <craigdmckeachie@gmail.com>
2018-01-10 15:34:12 -0500

    verified in release 5100

--- src/app/projects/project-card/project-card.component.spec.ts
+++ src/app/projects/project-card/project-card.component.spec.ts
@@ -1,10 +1,18 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ProjectCardComponent } from "./project-card.component";
+import { DebugElement } from "@angular/core";
+import { Project } from "../shared/project.model";
+import { By } from "@angular/platform-browser";
 
 describe("ProjectCardComponent", () => {
   let component: ProjectCardComponent;
   let fixture: ComponentFixture<ProjectCardComponent>;
+
+  let headerElement: HTMLElement;
+  let projectDetailDivElement: HTMLElement;
+  let editAnchorDebugElement: DebugElement;
+  let expectedProject: Project;
 
   beforeEach(
     async(() => {
@@ -17,10 +25,49 @@ describe("ProjectCardComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ProjectCardComponent);
     component = fixture.componentInstance;
+
+    headerElement = fixture.debugElement.query(By.css("h4")).nativeElement;
+    projectDetailDivElement = fixture.debugElement.query(
+      By.css("div.project-detail")
+    ).nativeElement;
+
+    editAnchorDebugElement = fixture.debugElement.query(By.css(".edit-action"));
+
+    expectedProject = new Project({
+      id: 15,
+      name: "Mission Impossible",
+      description: "This is really hard.",
+      editing: false
+    });
+    component.project = expectedProject;
     fixture.detectChanges();
   });
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("header should have project name", () => {
+    expect(headerElement.innerText).toEqual(expectedProject.name);
+  });
+
+  it("div should have project description", () => {
+    expect(projectDetailDivElement.textContent).toContain(
+      expectedProject.description
+    );
+  });
+
+  it("should raise edit event when edit clicked", () => {
+    let projectBeingEdited: Project;
+
+    component.edit.subscribe((project: Project) => {
+      projectBeingEdited = project;
+    });
+
+    editAnchorDebugElement.triggerEventHandler("click", {
+      preventDefault: () => {}
+    });
+
+    expect(projectBeingEdited).toBe(expectedProject);
   });
 });
