ae55431ee40ea978e0e511561d517b304ad3b356
Craig McKeachie <craigdmckeachie@gmail.com>
2018-01-10 13:31:18 -0500

    e2e testing

---
+++ e2e/projects.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { ProjectsPage } from "./projects.po";
+
+describe("Projects", () => {
+  let page: ProjectsPage;
+
+  beforeEach(() => {
+    page = new ProjectsPage();
+  });
+
+  it("should have projects", () => {
+    page.navigateTo();
+
+    expect(page.projectNameHeaders.count()).toEqual(6);
+  });
+
+  describe("when editing", () => {
+    beforeEach(() => {
+      page.navigateTo();
+      page.actionsButton.click();
+      page.editButton.click();
+    });
+
+    it("should show a form", () => {
+      expect(page.projectForm.isPresent()).toEqual(true);
+    });
+
+    it("should save updated project name", () => {
+      const updatedName = "updated project name";
+      page.updateName(updatedName);
+      page.saveButton.click();
+      expect(page.firstProjectHeader.getText()).toEqual(updatedName);
+    });
+  });
+});
